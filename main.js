84c8bf7a0c81bf6011532387d030aa3360783ea5
Celine10811020 <86948988+Celine10811020@users.noreply.github.com>
2021-07-07 18:02:49 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+var img = [];
+var i=0;
+var closeButton = [];
+
+function setup()
+{
+  noCanvas();
+  inputValue = createInput();
+
+  showImageButton = createButton("顯示圖片");
+  showImageButton.mouseClicked(showImage);
+
+  createP("");
+}
+
+
+
+function showImage()
+{
+  i = inputValue.value();
+  img[i] = createImg("images/"+ i +".png");
+
+  closeButton[i] = createButton("X", i);
+  closeButton[i].mouseClicked(closeImage);
+
+  inputValue.value("");
+};
+
+function closeImage()
+{
+  this.hide();
+  i = this.value();
+  img[i].hide();
+}
